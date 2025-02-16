e96b8918621eb4bc3554a38eb5bf6abcb3644a51
dheerajndhongade <dheerajndhongade@gmail.com>
2025-02-16 16:27:25 +0530

    Added redis cloud

--- backend/config/redis.js
+++ backend/config/redis.js
@@ -1,9 +1,6 @@
 const Redis = require("ioredis");
 
-const redis = new Redis({
-  host: process.env.REDIS_HOST,
-  port: process.env.REDIS_PORT,
-});
+const redis = new Redis(process.env.REDIS_URL);
 
 redis.on("error", (err) => {
   console.error("Redis error:", err);
